1c2c9527bac0500f464259fb80f6e008fccc535b
avinash122186 <avinash122186@gmail.com>
2018-11-20 11:13:23 +0530

    utility Functions

---
+++ src/ch02_linkedlists/Node.java
@@ -0,0 +1,13 @@
+package ch02_linkedlists;
+
+public class Node {
+	int data;
+	Node next;
+	
+	Node(int data)
+	{
+		this.data =data;
+		next =null;
+		}
+
+}
--- src/ch02_linkedlists/RemoveDups.java
+++ src/ch02_linkedlists/RemoveDups.java
@@ -1,5 +1,38 @@
 package ch02_linkedlists;
 
-public class RemoveDups {
-
+public class RemoveDups
+{
+	Node head;
+	public static void main(String args[])
+	{
+		
+	}
+	public static Node noDup(Node head)
+	{
+		Node n =head;
+		while(n != null)
+		{
+			
+		}
+		return head;
+	}
+	// Utility Functions
+		public void push(int new_data)
+		{
+			Node new_node = new Node(new_data);
+			new_node.next = head;
+			head = new_node;
+		}
+		
+		void printList() 
+	     { 
+	         Node temp = head; 
+	         while (temp != null) 
+	         { 
+	            System.out.print(temp.data+" "); 
+	            temp = temp.next; 
+	         }   
+	         System.out.println(); 
+	     } 
 }
+
